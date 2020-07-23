18ea058845e6645d4cfed2c9174b4a70331366f7
RocKing1001 <44892832+RocKing1001@users.noreply.github.com>
2020-07-23 12:18:59 +0200

    BODMAS
    but broken lol

--- fail.py
+++ fail.py
@@ -159,11 +159,89 @@ class Lexer:
 
 
 #######################################
+# Nodes
+#######################################
+
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+    
+    def __repr__(self):
+        return f'{self.tok}'
+
+class BinOpNode:
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+
+    def __repr__(self):
+        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+
+
+
+#######################################
+# Parser
+#######################################
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_index = 1
+        self.advance()
+
+    def advance(self):
+        self.tok_index += 1
+        if self.tok_index < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_index]
+        return self.current_tok
+
+    ####################################
+
+    def parse(self):
+        res = self.expr()
+        return res
+
+    def factor(self):
+        tok = self.current_tok
+
+        if tok.type in (TT_FLOAT, TT_INT):
+            self.advance()
+            return NumberNode(tok) 
+    
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self):
+        return self.bin_op(self.factor, (TT_PLUS, TT_MINUS))
+    
+    ####################################
+
+    def bin_op(self, function, ops):
+        left = function()
+
+        while self.current_tok.type in ops:
+            op_tok = self.current_tok
+            self.advance()
+            right = function()
+            left = BinOpNode(left, op_tok, right)
+
+        return left
+
+
+
+#######################################
 # RUN
 #######################################
 
 def run(fname, text):
+    #Generate tokens
     lexer = Lexer(fname, text)
     tokens, error = lexer.make_tokens()
+    if Error: return None, error
 
-    return tokens, error
+    #Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    return ast, error
